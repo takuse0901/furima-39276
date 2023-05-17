b63dd452c5498c47f3bb686fbd06137b6b5885ae
takuse0901 <sekitatech5856@gmail.com>
2023-05-17 22:25:48 +0900

    ユーザー登録・ログインログアウト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :name, presence: true
+  validates :sei, presence: true
+  validates :kana_name, presence: true
+  validates :kana_sei, presence: true
+  validates :birth, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
+
 end
