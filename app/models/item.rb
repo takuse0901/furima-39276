5756732b4aeb08a96fb566dd66443d91a1ab8685
takuse0901 <sekitatech5856@gmail.com>
2023-05-19 16:51:29 +0900

    activerecord,activehash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :delivery_day
+  belongs_to :prefecture
+
+  validates :item_name, presence: true
+  validates :caption, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :delivery_charge_id, numericality: { other_than: 1 }
+  validates :delivery_day_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  
 end
--- db/migrate/20230519062046_create_items.rb
+++ db/migrate/20230519062046_create_items.rb
@@ -1,9 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :item_name
-      t.text :caption
-      t.integer :price
+      t.string :item_name, null: false
+      t.text :caption, null: false
+      t.integer :price, null: false
+      t.integer :category_id, null: false
+      t.integer :condition_id, null: false
+      t.integer :delivery_charge_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :delivery_day_id, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
