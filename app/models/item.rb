e566507640948baf14493c645664a9e81c165e99
takuse0901 <sekitatech5856@gmail.com>
2023-05-19 19:35:11 +0900

    newアクション、createアクション実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,13 +11,14 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :caption, presence: true
-  validates :price, presence: true
+  validates :price, format: { with: /\A[0-9]+\z/ },
+  numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, presence: true
   validates :image, presence: true
 
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :delivery_charge_id, numericality: { other_than: 1 }
-  validates :delivery_day_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
 end
