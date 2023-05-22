f133a9712f7414133a00344447b4194f0b0a67d0
takuse0901 <sekitatech5856@gmail.com>
2023-05-22 19:59:59 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
      end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :caption, :category_id, :condition_id, :delivery_charge_id, :delivery_day_id, :prefecture_id, :price, :image).merge(user_id: current_user.id)
