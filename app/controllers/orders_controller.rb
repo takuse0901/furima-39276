df62dc91d1b890733d96221e3053f759b83c041e
takuse0901 <sekitatech5856@gmail.com>
2023-05-24 17:14:58 +0900

    購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,29 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
 
   def index
+    @order_address = OrderAddress.new
   end
-  
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :address, :building_name, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :address, :building_name, :tel, :order_id
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :address, :building_name, :tel
 
   with_options presence: true do
     validates :user_id
@@ -9,12 +9,11 @@ class OrderAddress
     validates :city
     validates :address
     validates :tel, format: {with: /\A\d{10,11}\z/, message: "is invalid."}
-    validates :order_id
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, order_id: order.id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, tel: tel, order_id: order.id)
   end
 end
