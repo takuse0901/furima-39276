6e1d0eecb7a9a75f3a320ea0745bf1483240a14f
takuse0901 <sekitatech5856@gmail.com>
2023-05-25 16:03:17 +0900

    テストコード追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -80,6 +80,11 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Item can't be blank")
       end
+      it 'tokenが空だと保存できない' do
+        @order_address.token = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
